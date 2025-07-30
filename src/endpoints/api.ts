0760dd2b8fef9c7f653af8e6b4d22a6881cf98b2
Lina Martinez <lmcastro3501@gmail.com>
2025-07-30 12:19:22 -0700

    Add API functions for user authentication, login and logout

---
+++ src/endpoints/api.ts
@@ -0,0 +1,87 @@
+import axios from "axios";
+
+const BASE_URL = 'https://vocabloom-backend.onrender.com/api/';
+const LOGIN_URL = `${BASE_URL}token/`;
+const REFRESH_URL = `${BASE_URL}token/refresh/`;
+const WORDS_URL = `${BASE_URL}words/`;
+const AUTH_URL = `${BASE_URL}authenticated/`;
+const LOGOUT_URL = `${BASE_URL}logout/`
+
+export const login = async (username: string, password: string): Promise<boolean> => {
+	const response = await axios.post<{ success: boolean }>(
+		LOGIN_URL,
+		{ username, password },
+		{ withCredentials: true }
+	);
+	return response.data.success;
+};
+
+export const refresh_token = async (): Promise<boolean> => {
+	try {
+		await axios.post<{ refreshed: boolean }>(
+			REFRESH_URL,
+			{},
+			{ withCredentials: true }
+		);
+		return true;
+	} catch (error) {
+		return false
+	}
+};
+
+export const get_words = async (): Promise<any[] | false> => {
+	try {
+		const response = await axios.get<any[]>(WORDS_URL, {
+			withCredentials: true,
+		});
+		return response.data;
+	} catch (error: any) {
+		return await call_refresh(error, () =>
+			axios.get<any[]>(WORDS_URL, { withCredentials: true })
+		);
+	}
+};
+
+type ErrorWithResponse = {
+	response?: {
+		status?: number;
+		[key: string]: any;
+	};
+	[key: string]: any;
+};
+
+export const call_refresh = async <T>(
+	error: ErrorWithResponse,
+	func: () => Promise<{ data: T }>
+): Promise<T | false> => {
+	if (error.response && error.response.status === 401) {
+		const tokenRefreshed = await refresh_token();
+		if (tokenRefreshed) {
+			const retryResponse = await func();
+			return retryResponse.data;
+		}
+	}
+	return false;
+};
+
+export const logout = async (): Promise<boolean> => {
+	try {
+		await axios.post(
+			LOGOUT_URL,
+			{},
+			{ withCredentials: true }
+		);
+		return true;
+	} catch (error) {
+		return false;
+	}
+};
+
+export const is_authenticated = async () => {
+	try {
+		await axios.post(AUTH_URL, {}, { withCredentials: true })
+		return true
+	} catch (error) {
+		return false
+	}
+}
