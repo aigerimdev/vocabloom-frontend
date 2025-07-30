5ac323ebfdf27bddbb059ea389e69bc3cf21a68e
Lina Martinez <lmcastro3501@gmail.com>
2025-07-30 12:21:14 -0700

    Refactor Logout component to implement logout functionality and improve user experience

--- src/pages/Logout.tsx
+++ src/pages/Logout.tsx
@@ -1,7 +1,20 @@
+import { useNavigate } from "react-router-dom";
+import { logout } from "../endpoints/api";
+import { useAuth } from "../context/useAuth";
+
 const Logout = () => {
+    const navigate = useNavigate();
+    const { setIsAuthenticated } = useAuth();
+
+    const handleLogout = async () => {
+        await logout();
+        setIsAuthenticated(false);
+        navigate('/welcome');
+    };
+
     return (
         <div>
-            <h1>Logout</h1>
+            <button onClick={handleLogout}>Log out</button>
         </div>
     );
 };
