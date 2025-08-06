83180c147b814a8b30e3288b6751fe13d457c740
Aigerim Kalygulova <kalygulovas@gmail.com>
2025-08-06 14:25:48 -0700

    adds style for bigger screens

--- src/pages/WelcomePage.tsx
+++ src/pages/WelcomePage.tsx
@@ -6,17 +6,20 @@ const WelcomePage = () => {
 
     return (
         <main className="welcome-page">
-            <section className="welcome-header">
-                <h1 className="welcome-title">
-                    Welcome to<br />
-                    <span className='welcome-title-highlight'>VocaBloom</span>
-                </h1>
-                <p className="welcome-subtitle">Every word you learn is a seed for your future</p>
-            </section>
-            <nav className="welcome-actions" aria-label="Authentication">
-                <button className="welcome-btn sign-in" onClick={() => navigate('/login')}>Sign in</button>
-                <button className="welcome-btn sign-up" onClick={() => navigate('/signup')}>Sign up</button>
-            </nav>
+            <div >
+                <section className="welcome-header">
+                    <h1 className="welcome-title">
+                        Welcome to<br />
+                        <span className='welcome-title-highlight'>VocaBloom</span>
+                    </h1>
+                    <p className="welcome-subtitle">Every word you learn is a seed for your future</p>
+                </section>
+                <nav className="welcome-actions" aria-label="Authentication">
+                    <button className="welcome-btn sign-in" onClick={() => navigate('/login')}>Sign in</button>
+                    <button className="welcome-btn sign-up" onClick={() => navigate('/signup')}>Sign up</button>
+                </nav>
+            </div>
+
             <footer className="welcome-footer">
                 <p>Made with <span role="img" aria-label="love">♥️</span> by Lina &amp; Aigerim</p>
                 <p>Ada Developers Academy c23</p>
