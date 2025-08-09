d705d2095fa9321b895563f972afd1260e0dba75
Aigerim Kalygulova <kalygulovas@gmail.com>
2025-08-08 18:57:30 -0700

    remove the Greet component

--- src/components/Greet.test.tsx
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import Greet from './Greet';
-
-test('renders greeting message', () => {
-    render(<Greet />);
-    const heading = screen.getByText(/hello, ada/i);
-    expect(heading).toBeInTheDocument();
-});
