ad9cf92b2f9165014b8e8edf7bf8ea883dd71012
Lina Martinez <lmcastro3501@gmail.com>
2025-08-09 19:12:30 -0700

    Refactor Navbar component to include FontAwesome icons and update styles for navigation links

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,8 +2,9 @@ import { Link, useNavigate } from 'react-router-dom';
 import '../styles/Navbar.css';
 import { logout } from "../endpoints/api";
 import { useAuth } from "../context/useAuth";
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-// import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faArrowRightFromBracket, faTag, faList } from '@fortawesome/free-solid-svg-icons'
+import { faHouse } from '@fortawesome/free-regular-svg-icons'
 
 
 const Navbar = () => {
@@ -23,9 +24,18 @@ const Navbar = () => {
                 VocaBloom <span>🌱</span>
             </Link>
             <div className="nav-links">
-                <Link to="/home">Home</Link>
-                <button onClick={handleLogout}>Log out</button>
-                {/* <FontAwesomeIcon icon={faEnvelope} /> */}
+                <Link to="/my-words" className='nav-links-words'>
+                    <FontAwesomeIcon icon={faList} className='nav-links-icons'/>
+                    <span>My Words</span>
+                </Link>
+                <Link to="/tags" className='nav-links-tags'>
+                    <FontAwesomeIcon icon={faTag} className='nav-links-icons'/>
+                    <span>My Tags</span>
+                </Link>
+                <Link to="/home" aria-label="Home">
+                    <FontAwesomeIcon icon={faHouse} className='navbar-icons'/>
+                </Link>
+                <button onClick={handleLogout} aria-label='Log out'><FontAwesomeIcon icon={faArrowRightFromBracket} /></button>
             </div>
         </nav>
     );
